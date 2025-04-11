90de4e0278c243faf6fb242274a545e383ba90bb
Berkay Dedeoğlu <berkay.dedeoglu@outlook.com>
2025-04-11 22:50:28 +0300

    fix env variables

--- nomad-deployer/env.sh
+++ nomad-deployer/env.sh
@@ -5,4 +5,5 @@ export GITHUB_BRANCH="${NOMAD_VAR_GITHUB_BRANCH}"
 export NOMAD_ADDR="${NOMAD_VAR_NOMAD_ADDR}"
 export NOMAD_ACL_TOKEN="${NOMAD_VAR_NOMAD_ACL_TOKEN}"
 
+cd /app
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
